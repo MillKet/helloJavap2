09549acba54f2e3e9317cfd20e2e5c7ead74c336
Milliyon <milliyon.ketera@gmail.com>
2021-03-03 18:28:14 -0600

    my 2nd comit

---
+++ src/com/Jhello/JavaWorld2.java
@@ -0,0 +1,10 @@
+package com.Jhello;
+
+public class JavaWorld2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Miki is in timeout");
+	}
+
+}
